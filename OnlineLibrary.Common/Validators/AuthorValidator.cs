5ef87416477acfb50f5f83fe9c240a8f3d93f82f
KseniyaOsad <ksy.osad@gmail.com>
2022-01-19 18:28:44 +0200

    add validators

---
+++ OnlineLibrary.Common/Validators/AuthorValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using OnlineLibrary.Common.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineLibrary.Common.Validators
+{
+    public class AuthorValidator : AbstractValidator<Author>
+    {
+        public AuthorValidator()
+        {
+            RuleFor(x => x.Id).NotNull();
+            RuleFor(x => x.Name).NotEmpty();
+        }
+    }
+}
