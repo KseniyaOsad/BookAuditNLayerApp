ee844195ea0fe080b6f56763f4ba1785a82a6ccf
KseniyaOsad <ksy.osad@gmail.com>
2022-01-20 17:39:29 +0200

    Add class ErrorHandlerMiddleware, enum ExceptionType

---
+++ OnlineLibrary.Common/Exceptions/Enum/ExceptionType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineLibrary.Common.Exceptions.Enum
+{
+    public enum ExceptionType
+    {
+        BadRequest = 400,
+        Unauthorized = 401,
+        NotFound = 404,
+        InternalServerError = 500
+    }
+}
---
+++ OnlineLibrary.Common/Exceptions/OLException.cs
@@ -0,0 +1,14 @@
+﻿using OnlineLibrary.Common.Exceptions.Enum;
+using System;
+
+namespace OnlineLibrary.Common.Exceptions
+{
+    public class OLException : Exception
+    {
+        public ExceptionType Property { get; protected set; }
+        public OLException(string message, ExceptionType prop) : base(message)
+        {
+            Property = prop;
+        }
+    }
+}
