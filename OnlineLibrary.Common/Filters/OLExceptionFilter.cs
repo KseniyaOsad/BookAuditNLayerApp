edaa3ce1cb4f76f8debf3a14d3b31d95caa02d47
KseniyaOsad <ksy.osad@gmail.com>
2022-01-21 13:21:33 +0200

    Add OLExceptionFilter

---
+++ OnlineLibrary.Common/Filters/OLExceptionFilter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using OnlineLibrary.Common.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineLibrary.Common.Filters
+{
+    public class OLExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            Exception exc = context.Exception;
+            var olException = exc as OLException;
+
+            context.Result = new ContentResult
+            {
+                Content = exc.ToString(),
+                StatusCode = olException == null ? 500 : (int)olException.Property
+            };
+        }
+    }
+}
