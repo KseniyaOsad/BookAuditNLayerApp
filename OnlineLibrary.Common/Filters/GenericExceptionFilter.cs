9208c14f356aa27102079b7dfe2d82f52dd4645f
KseniyaOsad <ksy.osad@gmail.com>
2022-01-21 16:19:38 +0200

    add dynamic

--- OnlineLibrary.Common/Filters/GenericExceptionFilter.cs
+++ OnlineLibrary.Common/Filters/GenericExceptionFilter.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using OnlineLibrary.Common.Exceptions;
 using System;
@@ -11,23 +12,26 @@ namespace OnlineLibrary.Common.Filters
     {
         public void OnException(ExceptionContext context)
         {
-            switch (context.Exception)
+            HandleException((dynamic)context.Exception, context);
+            context.ExceptionHandled = true;
+        }
+
+        private void HandleException(Exception exception, ExceptionContext context)
+        {
+            context.Result = new ContentResult
             {
-                case OLException exc:
-                    context.Result = new ContentResult
-                    {
-                        Content = exc.ToString(),
-                        StatusCode = (int)exc.Property
-                    };
-                    break;
-                default:
-                    context.Result = new ContentResult
-                    {
-                        Content = context.Exception.ToString(),
-                        StatusCode = 505
-                    };
-                    break;
-            }
+                Content = context.Exception.ToString(),
+                StatusCode = StatusCodes.Status500InternalServerError
+            };
+        }
+
+        private void HandleException(OLException exception, ExceptionContext context)
+        {
+            context.Result = new ContentResult
+            {
+                Content = exception.ToString(),
+                StatusCode = (int)exception.Property
+            };
         }
     }
 }
