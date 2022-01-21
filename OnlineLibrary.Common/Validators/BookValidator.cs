d0178547d9be6c71289f9c7260ad4caacd9fd4c3
KseniyaOsad <ksy.osad@gmail.com>
2022-01-21 13:21:04 +0200

    Add book validator

---
+++ OnlineLibrary.Common/Validators/BookValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using OnlineLibrary.Common.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineLibrary.Common.Validators
+{
+    public class BookValidator : AbstractValidator<Book>
+    {
+        public BookValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty();
+            RuleFor(x => x.Description).NotEmpty();
+            RuleFor(x => x.Authors).NotEmpty();
+            RuleFor(x => x.Genre).NotEmpty().IsInEnum();
+            //RuleFor(x => x.Tags).NotEmpty();
+        }
+    }
+}
