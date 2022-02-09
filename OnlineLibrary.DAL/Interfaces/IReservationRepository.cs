19556f3779ed3ed1d3bac50bdd814574d8f5277f
KseniyaOsad <ksy.osad@gmail.com>
2022-02-09 13:50:24 +0200

    Fixed, Add new functionality

---
+++ OnlineLibrary.DAL/Interfaces/IReservationRepository.cs
@@ -0,0 +1,19 @@
+﻿using OnlineLibrary.Common.DBEntities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace OnlineLibrary.DAL.Interfaces
+{
+    public interface IReservationRepository
+    {
+        Task<List<Reservation>> GetAllReservationsAsync();
+
+        Task<List<Reservation>> GetBookReservationHistoryAsync(int bookId);
+
+        Task<List<Reservation>> GetUserReservationHistoryAsync(int userId);
+
+        Task CreateReservationAsync(Reservation reservation);
+
+        Task CloseReservationAsync(Reservation reservation);
+    }
+}
---
+++ OnlineLibrary.DAL/Interfaces/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using OnlineLibrary.Common.DBEntities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace OnlineLibrary.DAL.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<List<User>> GetAllUsersAsync();
+
+        Task CreateUserAsync(User user);
+
+        Task<bool> IsUserExistAsync(int userId);
+    }
+}
