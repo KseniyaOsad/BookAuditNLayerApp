7c4d28d50a5d9f19ba7dd5aa900e0e7868f0a29e
KseniyaOsad <ksy.osad@gmail.com>
2022-02-09 13:42:06 +0200

    Add CreateUser and ReservationModel classes

---
+++ OnlineLibrary.API/Model/CreateUser.cs
@@ -0,0 +1,15 @@
+﻿namespace OnlineLibrary.API.Model
+{
+    public class CreateUser
+    {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public int Year { get; set; }
+
+        public int Month { get; set; }
+
+        public int Day { get; set; }
+    }
+}
