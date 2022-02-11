c07a15728099579630d167d40acf02086a9f66cb
KseniyaOsad <ksy.osad@gmail.com>
2022-02-11 11:54:06 +0200

    Create Tables, create pre- and post- deployment scripts

---
+++ OnlineLibraryDB/Tables/Books.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Books]
+(
+	[Id] INT IDENTITY (1, 1) NOT NULL PRIMARY KEY, 
+    [Name] NVARCHAR(250) NOT NULL, 
+    [Description] TEXT NOT NULL, 
+    [InArchive] BIT NOT NULL DEFAULT 0, 
+    [Genre] TINYINT NOT NULL, 
+    [RegistrationDate] DATETIME2 NOT NULL DEFAULT GETDATE()
+)
