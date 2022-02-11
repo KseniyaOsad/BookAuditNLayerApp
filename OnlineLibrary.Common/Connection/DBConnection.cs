c97607578f2a35369c874b74aa07ab3dd9e9eeed
KseniyaOsad <ksy.osad@gmail.com>
2022-02-11 10:01:27 +0200

    Small fixes

---
+++ OnlineLibrary.Common/Connection/DBConnection.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineLibrary.Common.Connection
+{
+    public class DBConnection
+    {
+        public const string ConnectionStrings = "ConnectionStrings";
+
+        public string BookContext { get; set; } = string.Empty;
+    }
+}
--- OnlineLibrary.Common/Validators/UserValidator.cs
+++ OnlineLibrary.Common/Validators/UserValidator.cs
@@ -1,5 +1,6 @@
 ﻿using FluentValidation;
 using OnlineLibrary.Common.DBEntities;
+using System;
 
 namespace OnlineLibrary.Common.Validators
 {
@@ -9,7 +10,7 @@ namespace OnlineLibrary.Common.Validators
         {
             RuleFor(x => x.Name).NotEmpty();
             RuleFor(x => x.Email).NotEmpty().EmailAddress();
-            RuleFor(x => x.DateOfBirth).NotEmpty();
+            RuleFor(x => x.DateOfBirth).NotEmpty().InclusiveBetween(new DateTime(1900, 1, 1), DateTime.Now);
         }
     }
 }
