fa1dc2c30aa13281bc599c83d312bc0f17a809ad
KseniyaOsad <ksy.osad@gmail.com>
2022-02-11 11:54:06 +0200

    Create Tables, create pre- and post- deployment scripts

---
+++ OnlineLibraryDB/Tables/AuthorBook.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[AuthorBook]
+(
+	[Id] INT IDENTITY (1, 1) NOT NULL PRIMARY KEY, 
+    [BooksId] INT NOT NULL, 
+    [AuthorsId] INT NOT NULL, 
+    CONSTRAINT [FK_AuthorBook_Authors_AuthorsId] FOREIGN KEY ([AuthorsId]) REFERENCES [Authors]([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_AuthorBook_Books_BooksId] FOREIGN KEY ([BooksId]) REFERENCES [Books]([Id]) ON DELETE CASCADE
+)
