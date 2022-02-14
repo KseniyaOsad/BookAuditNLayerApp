39c05a50f62a09eeab0c32437cd6ed914725eeb9
KseniyaOsad <ksy.osad@gmail.com>
2022-02-14 13:50:54 +0200

    Fix pre- and post- deployment scripts

---
+++ OnlineLibraryDB/POST-Deployment/Post-Seed-Books.sql
@@ -0,0 +1,4 @@
+﻿SET IDENTITY_INSERT Books ON 
+INSERT INTO Books (Id, Name, Description, Genre) 
+VALUES (1, 'c# 7.0 in a nutshell', 'the definitive reference', 8), (2, 'Harry Potter', ' The boy who lived', 3);
+SET IDENTITY_INSERT Books OFF ;
---
+++ OnlineLibraryDB/POST-Deployment/Post-Seed-Users.sql
@@ -0,0 +1,4 @@
+﻿SET IDENTITY_INSERT Users ON
+INSERT INTO Users (Id, Name, Email, DateOfBirth) 
+VALUES (1, 'Viktor', 'victor@gmail.com', '1999-01-01'), (2, 'Anya', 'anya@gmail.com', '1989-11-10');
+SET IDENTITY_INSERT Users OFF;
