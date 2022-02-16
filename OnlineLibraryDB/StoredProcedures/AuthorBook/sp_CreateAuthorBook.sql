e474f31405d4a310c7edb491d92decc996245059
KseniyaOsad <ksy.osad@gmail.com>
2022-02-16 14:09:25 +0200

    Add (delete_, create_, get_) procedures for BookTag and AuthorBook Entities

---
+++ OnlineLibraryDB/StoredProcedures/AuthorBook/sp_CreateAuthorBook.sql
@@ -0,0 +1,6 @@
+﻿CREATE PROCEDURE [dbo].[sp_CreateAuthorBook]
+	@bookId int,
+	@authorId int
+AS
+	SET NOCOUNT ON;
+	INSERT INTO [dbo].[AuthorBook] (BooksId, AuthorsId) VALUES (@bookId, @authorId);
---
+++ OnlineLibraryDB/StoredProcedures/AuthorBook/sp_DeleteAuthorBook.sql
@@ -0,0 +1,6 @@
+﻿CREATE PROCEDURE [dbo].[sp_DeleteAuthorBook]
+	@bookId int,
+	@authorId int
+AS
+	SET NOCOUNT ON;
+	DELETE FROM [dbo].[AuthorBook] WHERE BooksId = @bookId AND AuthorsId = @authorId;
